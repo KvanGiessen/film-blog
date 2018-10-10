eb8bad625847f91c033e1cd3a5874bdd87231f66
Kevin van Giessen <kvangiessen@gmail.com>
2018-10-10 23:18:22 +0100

    added html parser

--- app/find_film_data.py
+++ app/find_film_data.py
@@ -6,7 +6,7 @@ def get_img_src(query):
     print(url)
     res = requests.get(url)
     res.raise_for_status()
-    soup = BeautifulSoup(res.text)
+    soup = BeautifulSoup(res.text, "html.parser")
     first_img = soup.find('img')
     src = first_img.get('src')
     return src
@@ -16,7 +16,7 @@ def get_imdb_src(query):
     print(url)
     res = requests.get(url)
     res.raise_for_status()
-    soup = BeautifulSoup(res.text)
+    soup = BeautifulSoup(res.text, "html.parser")
     elements = soup.select('.r a')
     src = elements[0].get('href')[7:]
     link = src.split('&')[0]
