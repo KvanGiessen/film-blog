ed3694db6e4c3ca36af752e709e049efd9d9b2da
Kevin van Giessen <kvangiessen@gmail.com>
2018-10-10 19:40:04 +0100

    Added all files

---
+++ app/find_film_data.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import urllib3
+import json
+import requests
+
+def get_img_src(query):
+    url = """https://www.google.com/images?q='%s'""" % query
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text)
+    first_img = soup.find('img')
+    src = first_img.get('src')
+    return src
+
+def get_imdb_src(query):
+    url = """https://www.google.com/search?q='%s imdb'""" % query
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text)
+    elements = soup.select('.r a')
+    src = elements[0].get('href')[7:]
+    link = src.split('&')[0]
+    return link
+
+if __name__ == '__main__':
+    print(get_imdb_src('shawshank redemption'))
