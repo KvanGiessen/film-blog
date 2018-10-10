2b886cb465510038bbf039bd42642d2ed7ab0e9b
Kevin van Giessen <kvangiessen@gmail.com>
2018-10-10 23:30:22 +0100

    removed searchable films

--- app/models.py
+++ app/models.py
@@ -252,8 +252,8 @@ class Post(SearchableMixin, db.Model):
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
-class Film(SearchableMixin, db.Model):
-    __searchable__ = ['title']
+class Film(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
