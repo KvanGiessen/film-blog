36716b0ff1fbd7a1e7f61d032f09c316da541853
Kevin van Giessen <kvangiessen@gmail.com>
2018-10-10 23:13:02 +0100

    added print url commands

--- app/find_film_data.py
+++ app/find_film_data.py
@@ -3,6 +3,7 @@ import requests
 
 def get_img_src(query):
     url = """https://www.google.com/images?q='%s'""" % query
+    print(url)
     res = requests.get(url)
     res.raise_for_status()
     soup = BeautifulSoup(res.text)
@@ -12,6 +13,7 @@ def get_img_src(query):
 
 def get_imdb_src(query):
     url = """https://www.google.com/search?q='%s imdb'""" % query
+    print(url)
     res = requests.get(url)
     res.raise_for_status()
     soup = BeautifulSoup(res.text)
